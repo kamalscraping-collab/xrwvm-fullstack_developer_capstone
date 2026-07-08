4e9ba1e6e0378eb84c9fd041c97152b5d3c54550
kamalscraping-collab <kamalscraping@gmail.com>
2026-07-08 01:03:29 -0400

    Fixed code to satisfy the linters

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -121,12 +121,13 @@ def get_dealer_details(request, dealer_id):
 # def add_review(request):
 # ...
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
+            print(response)
             return JsonResponse({"status":200})
-        except:
+        except Exception as e:
             return JsonResponse({"status":401,"message":"Error in posting review"})
     else:
         return JsonResponse({"status":403,"message":"Unauthorized"})
